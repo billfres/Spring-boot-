1047f02ed2fbedcbc7e48f1ac563019d2aa7a839
billfres <ngomafresnel@gmail.com>
2021-12-11 03:44:43 +0100

    Added of PostRepository and TagRepository

---
+++ Spring-Boot-Many-to-Many-Mapping-jpa-Hibernate/src/main/java/com/fresnel/cg/models/Repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.fresnel.cg.models.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fresnel.cg.models.Tag;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
