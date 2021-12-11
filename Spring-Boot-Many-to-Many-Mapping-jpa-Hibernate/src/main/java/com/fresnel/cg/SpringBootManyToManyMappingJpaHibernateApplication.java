4ced65dccc065f0f233ec0382108d4644bdf006e
billfres <ngomafresnel@gmail.com>
2021-12-11 02:30:21 +0100

    First commit

---
+++ Spring-Boot-Many-to-Many-Mapping-jpa-Hibernate/src/main/java/com/fresnel/cg/SpringBootManyToManyMappingJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.fresnel.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToManyMappingJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootManyToManyMappingJpaHibernateApplication.class, args);
+	}
+
+}
